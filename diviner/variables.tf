d78353b234678d4eac1a327fdefb18553cd24628
Michael Simo <os@mpmsimo.com>
2019-04-11 19:09:59 -0700

    Deploy kubernetes fork

---
+++ diviner/variables.tf
@@ -0,0 +1,10 @@
+variable "region" {
+  type        = "string"
+  default     = "us-east1"
+  description = "Google Cloud Platform region"
+}
+
+variable "service" {
+  type    = "string"
+  default = "nsk"
+}
