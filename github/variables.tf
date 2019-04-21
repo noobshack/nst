a7f83bc27dd493828b0ea6eabc53ec38625c7e5b
Michael Simo <os@mpmsimo.com>
2019-04-21 14:36:58 -0700

    Initialize and plan all existing Terraform repositories

--- github/variables.tf
+++ github/variables.tf
@@ -1,18 +1,28 @@
+// Injected secrets
+variable "github_token" {}
+
+variable "github_webhook_secret_nst" {}
+
+variable "discord_token" {
+  type    = "string"
+  default = "pqrCQkBNugpDilBCTvzcqY31tPcWGHUjMJ22KLOlkIwS_tDq8dSJvSYN-y5ZNUnJhUh9"
+}
+
+// Provider variables
+variable "github_organization" {
+  type    = "string"
+  default = "noobshack"
+}
+
 // Member lists
 variable "github_owners" {
   description = "List of noobshack owners."
   type        = "list"
-  default     = "${local.github_owner_list}"
+  default     = ["mpmsimo", "noobshackbot"]
 }
 
 variable "github_members" {
   description = "List of noobshack members."
   type        = "list"
-  default     = "${local.github_member_list}"
+  default     = ["mzamani1", "rangoris", "cynize", "jrsimo91"]
 }
-
-locals {
-  github_owner_list = ["mpmsimo", "noobshackbot"]
-  github_member_list = ["mzamani1", "rangoris", "cynize", "jrsimo91"]
-}
-
